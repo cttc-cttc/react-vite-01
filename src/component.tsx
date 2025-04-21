34249e8ffeb7873c451012bef204ae0cfa0ace7b
kwy <kwy1762@naver.com>
2025-04-21 13:16:06 +0900

    component 생성 실습

---
+++ src/component.tsx
@@ -0,0 +1,10 @@
+const Component = () => {
+    return (
+        <div>
+            <h1>hello, react!</h1>
+            <p>리액트는 ui를 만드는 라이브러리입니다</p>
+        </div>
+    )
+}
+
+export default Component;
